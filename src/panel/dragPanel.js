0820d0ad3d44b6fec49e1e320c55a147a1141a1b
Sean Feng <sean@seanfeng.dev>
2019-12-13 23:33:59 -0500

    Update image dropping UI (#4)

--- src/panel/dragPanel.js
+++ src/panel/dragPanel.js
@@ -1,12 +1,43 @@
-function dragenter(e) {
-  e.stopPropagation();
-  e.preventDefault();
+function getExtension(filename) {
+    var parts = filename.split('.');
+    return parts[parts.length - 1];
 }
 
-function dragover(e) {
-  e.stopPropagation();
-  e.preventDefault();
+function isImage(filename) {
+    var ext = getExtension(filename);
+    switch (ext.toLowerCase()) {
+    case 'jpg':
+    case 'jpeg':
+    case 'gif':
+    case 'png':
+        //etc
+        return true;
+    }
+    return false;
 }
+
+const dropzone = document.querySelector("#drop_zone");
+
+dropzone.addEventListener('dragover', function (e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+    this.style.background = "grey";
+}, false);
+
+dropzone.addEventListener('dragenter', function (e) {
+    e.dataTransfer.dropEffect = 'copy';
+    e.preventDefault();
+});
+
+dropzone.addEventListener('dragleave', function () {
+    this.style.background = "white";
+});
+
+dropzone.addEventListener('drop', function (e) {
+    this.style.background = "white";
+}, false);
+
+dropzone.addEventListener('drop', drop, false);
 
 var dbMAG;
 // Ask the background script to update it's cache
@@ -17,59 +48,64 @@ const pingUpdate = () => {
 };
 
 const updateMessage = message => {
-  const curInfoElement = document.getElementById("drop_zone_label");
   const newInfoElement = document.createElement("p");
   const node = document.createTextNode(message);
   newInfoElement.appendChild(node);
-
-  document.body.replaceChild(newInfoElement, curInfoElement);
+  document.body.appendChild(newInfoElement);
 };
+
+function removeEvent() {
+  document.querySelector("#drop_zone").removeEventListener('drop', drop, false);
+}
 
 window.onload = function() {
   dbMAG = new IndexedDBWrapper();
   dbMAG.openDB(function(ret) {
   });
-
-  const dropzone = document.getElementById("drop_zone");
-  dropzone.addEventListener(
-    "dragenter dragstart dragend dragleave dragover drag drop",
-    function(e) {
-      e.preventDefault();
-    }
-  );
-
-  dropzone.addEventListener("dragenter", dragenter, false);
-  dropzone.addEventListener("dragover", dragover, false);
-  dropzone.addEventListener("drop", drop, false);
 };
 
+const removeDropZone = () => {
+  const dropZone = document.getElementById("drop_zone");
+  dropZone.parentNode.removeChild(dropZone);
+}
+
 function drop(e) {
-  e.stopPropagation();
   e.preventDefault();
   const urlParams = new URLSearchParams(window.location.search);
   const sitePattern = urlParams.get("sitePattern");
   const mode = urlParams.get("mode");
-  const file = e.dataTransfer.files[0];
 
-  if (!file) {
-    //TODO: Figure out why an image may be empty here.
+  const files = e.target.files || e.dataTransfer.files;
+
+  if (files[0]) {
+    removeEvent();
+  }
+
+  //TODO: Figure out why an image may be empty here.
+  if (!files[0]) {
     updateMessage(
-      "Failed to upload the image, please close this window and try again"
+      "Failed to upload the image, please close this window and try again."
     );
+    removeDropZone();
     return;
   }
 
-  const data = genUploadData(sitePattern, file, true);
+  if (!isImage(files[0].name)) {
+    updateMessage(
+      "This is not a image file, please upload an image file."
+    );
+    removeDropZone();
+    return;
+  }
+
+  const data = genUploadData(sitePattern, files[0], true);
 
   dbMAG.upsert(data, function(e) {
     if (e === sitePattern) {
       // Uploaded Successfully
       updateMessage("Upload Success, Please close this window");
       pingUpdate();
-
-      // Remove the dropZone
-      const dropZone = document.getElementById("drop_zone");
-      dropZone.parentNode.removeChild(dropZone);
+      removeDropZone();
     }
   });
 }
